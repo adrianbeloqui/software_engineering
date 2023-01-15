4055aef14d565188574829408eebce77e954ec69
Adrian Beloqui <adribeloqui@gmail.com>
2023-01-15 01:04:05 +0000

    New approach without using lists

--- Data Structures/Array and Strings/p03_urlify.py
+++ Data Structures/Array and Strings/p03_urlify.py
@@ -65,3 +65,29 @@ def urlify3(string: str, length: str) -> str:
 
 assert urlify3("Mr John Smith     ", 13) == 'Mr%20John%20Smith'
 assert urlify3("this is another test     ", 20) == 'this%20is%20another%20test'
+
+
+def urlify4(string: str, length: str) -> str:
+    """4th approach
+
+    O(n)
+    """
+    space_count = 0
+    for i in range(0, length):
+        if string[i] == " ":
+            space_count += 1
+
+    # we need 2 more slots for each space found to change a space for %20
+    final_length = length + (space_count * 2)
+
+    cursor = 0
+    while cursor < final_length:
+        if string[cursor] == " ":
+            string = string[0:cursor] + "%20" + string[cursor+1:]
+            cursor += 3
+        else:
+            cursor += 1
+    return string[:final_length]
+
+assert urlify4("Mr John Smith     ", 13) == 'Mr%20John%20Smith'
+assert urlify4("this is another test     ", 20) == 'this%20is%20another%20test'

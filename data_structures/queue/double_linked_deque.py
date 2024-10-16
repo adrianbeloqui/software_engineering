3b1404a25b2be1f78bd94ab89ee7204c18150342
Adrian Beloqui <adribeloqui@gmail.com>
2024-10-16 22:14:18 +0000

    Add double linked list base class and double linked deque class

---
+++ data_structures/queue/double_linked_deque.py
@@ -0,0 +1,30 @@
+from data_structures.linked_lists.double_linked_base import _DoubleLinkedBase
+from data_structures.exceptions import Empty
+
+
+class DoubleLinkedDeque(_DoubleLinkedBase):
+    def first(self):
+        if self.is_empty():
+            raise Empty("Deque is empty")
+        return self._header._next._element
+    
+    def last(self):
+        if self.is_empty():
+            raise Empty("Deque is empty")
+        return self._trailer._prev._element
+    
+    def insert_first(self, e):
+        return self._insert_between(e, self._header, self._header._next)
+    
+    def insert_last(self, e):
+        return self._insert_between(e, self._trailer._prev, self._trailer)
+    
+    def delete_first(self):
+        if self.is_empty():
+            raise Empty("Deque is empty")
+        return self._delete_node(self._header._next)
+    
+    def delete_last(self):
+        if self.is_empty():
+            raise Empty("Deque is empty")
+        return self._delete_node(self._trailer._prev)

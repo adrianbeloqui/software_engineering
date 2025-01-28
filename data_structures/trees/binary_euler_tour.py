1fd5d0ceeb175c0eeb02df26f1c4ba4a2620828b
Adrian Beloqui <adribeloqui@gmail.com>
2025-01-28 01:17:31 +0000

    Fix typo

--- data_structures/trees/binary_euler_tour.py
+++ data_structures/trees/binary_euler_tour.py
@@ -18,16 +18,16 @@ class BinaryEulerTour(EulerTour):
             path.append(0)
             results[0] = self._tour(self._tree.left(p), d + 1, path)
             path.pop()
-        
+
         self._hook_invisit(p, d, path)
 
         if self._tree.right(p) is not None:
             path.append(1)
-            results[0] = self._tour(self._tree.right(p), d + 1, path)
+            results[1] = self._tour(self._tree.right(p), d + 1, path)
             path.pop()
-        
+
         answer = self._hook_postvisit(p, d, path, results)
         return answer
-    
+
     def _hook_invisit(self, p, d, path):
         pass
